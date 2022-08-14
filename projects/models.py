f11d0fdd37cf31f9b7b6397277c69c5adf79c636
System Administrator <root@Changs-MacBook-Pro.local>
2022-08-14 21:11:56 +0800

    Database Queries

--- projects/models.py
+++ projects/models.py
@@ -9,8 +9,33 @@ class Projects(models.Model):
     description = models.TextField(null=True, blank=True) 
     demo_link = models.CharField(max_length=2000, null=True, blank=True)
     source_link = models.CharField(max_length=2000, null=True, blank=True)
+    tags = models.ManyToManyField('Tag', blank=True)
+    vote_total = models.IntegerField(default=0, null=True, blank=True)
+    vote_ratio = models.IntegerField(default=0, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
     
+class Review(models.Model):
+    VOTE_TYPE = (
+        ('up', 'Up Vote'),
+        ('down', 'Down Vote'),
+    )
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    # owner = 
+    projects = models.ForeignKey(Projects, on_delete=models.CASCADE) # 關聯的Projects被刪除的話，相關的Review相會自動被刪除
+    body = models.TextField(null=True, blank=True)
+    value = models.CharField(max_length=200, choices=VOTE_TYPE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.value
+
+class Tag(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
