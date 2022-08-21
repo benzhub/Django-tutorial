0cac2829519adbc3256d38f7ad293eecb8640b76
System Administrator <root@Changs-MacBook-Pro.local>
2022-08-21 17:24:30 +0800

    Add Render Profiles

--- users/views.py
+++ users/views.py
@@ -1,6 +1,20 @@
+from pydoc import describe
 from django.shortcuts import render
-
-# Create your views here.
+from .models import Profile
 
 def profiles(request):
-    return render(request, 'users/profiles.html')
+    profiles = Profile.objects.all()
+    context = {"profiles": profiles}
+    return render(request, 'users/profiles.html', context)
+
+def userProfile(request, pk):
+    profile = Profile.objects.get(id=pk)
+    topSkills   = profile.skill_set.exclude(description__exact="") # 排除描述為空值的description
+    otherSkills = profile.skill_set.filter(description="") # 將描述為空值的description放進other Skill
+
+    context= {
+        "profile": profile,
+        "topSkills": topSkills,
+        "otherSkills": otherSkills
+    }
+    return render(request, 'users/user-profile.html', context)
