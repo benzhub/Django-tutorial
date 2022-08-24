25f262d5d2df5e4c7b7677473af5350ab819c0a7
System Administrator <root@Changs-MacBook-Pro.local>
2022-08-24 18:37:46 +0800

    sending welcome emails

--- users/signals.py
+++ users/signals.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import User
 from .models import Profile
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
+from django.core.mail import send_mail
+from django.conf import settings
 
 # example
 @receiver(post_save, sender=Profile)
@@ -36,6 +38,16 @@ def createProfile(sender, instance, created, **kwargs):
             email=user.email,
             name=user.first_name
             )
+        subject = "Welcome to DevSearch"
+        message = "We are glad you are here!"
+
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            [profile.email],
+            fail_silently=False,
+        )
 
 def updateProfile(sender, instance, created, **kwargs):
     profile = instance
