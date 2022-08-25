e1d992b9d8772afc00d6d22a91a08cce900d700d
System Administrator <root@Changs-MacBook-Pro.local>
2022-08-25 21:52:13 +0800

    Sending Post Requests

--- api/views.py
+++ api/views.py
@@ -1,9 +1,10 @@
+from curses import erasechar
 from django.http import JsonResponse
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 from .serializers import ProjectSerializer
-from projects.models import Projects as Project
+from projects.models import Projects as Project, Review
 
 @api_view(["GET"])
 def getRoutes(request):
@@ -31,4 +32,24 @@ def getProjects(request):
 def getProject(request, pk):
     project = Project.objects.get(id=pk)
     serializer = ProjectSerializer(project, many=False) # 返回1個就要設定many=False
+    return Response(serializer.data)
+
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def projectVote(request, pk):
+    project = Project.objects.get(id=pk)
+    user = request.user.profile
+    data = request.data
+
+    # print(f"DATA: {data}")
+    review, created = Review.objects.get_or_create(
+        owner = user,
+        projects = project,
+    )
+
+    review.value = data["value"]
+    review.save()
+    project.getVoteCount
+
+    serializer = ProjectSerializer(project, many=False)
     return Response(serializer.data)
